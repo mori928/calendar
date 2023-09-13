e4cdb28c55c793b5ff8e540fb61ad196723953f1
mori928 <mori.nutrition@gmail.com>
2023-09-13 12:10:52 +0900

    First Commit

---
+++ app/controllers/yoyakus_controller.rb
@@ -0,0 +1,44 @@
+class YoyakusController < ApplicationController
+  def index
+    @yoyakus = Yoyaku.all
+    @yoyaku = Yoyaku.new
+  end
+  
+  def new
+    @yoyaku = Yoyaku.new
+  end
+
+  def show
+    @yoyaku = Yoyaku.find(params[:id])
+  end
+
+  def create
+    Yoyaku.create(yoyaku_parameter)
+    redirect_to yoyakus_path
+  end
+
+  def destroy
+    @yoyaku = Yoyaku.find(params[:id])
+    @yoyaku.destroy
+    redirect_to yoyakus_path, notice:"削除しました"
+  end
+
+  def edit
+    @yoyaku = Yoyaku.find(params[:id])
+  end
+
+  def update
+    @yoyaku = Yoyaku.find(params[:id])
+    if @yoyaku.update(yoyaku_parameter)
+      redirect_to yoyakus_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def yoyaku_parameter
+    params.require(:yoyaku).permit(:title, :content, :start_time)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    root to: 'yoyakus#index'
+    resources :yoyakus
+  
+  end
+end
